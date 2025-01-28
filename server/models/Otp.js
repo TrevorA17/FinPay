656589c8d16bb92e67879e4704d86baeffd99bb9
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2025-01-28 15:07:25 +0300

    model debug

--- server/models/Otp.js
+++ server/models/Otp.js
@@ -5,6 +5,7 @@ const OtpSchema = new mongoose.Schema(
     email: { type: String, required: true },
     otp: { type: String, required: true },
     createdAt: { type: Date, default: Date.now, expires: 300 }, // OTP expires in 5 minutes
+    expiresAt: { type: Date, required: true },
   },
   { collection: "otps" } // Explicitly specify the collection name
 );
