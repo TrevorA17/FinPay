e10504b89d05c8da4d8cc5ef980cfb25e89be108
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2025-01-28 15:00:43 +0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String, required: true },
-  password: { type: String, required: true },
-});
-
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    password: { type: String, required: true },
+    isVerified: {
+      type: Boolean,
+      default: false, // Initially, the user is not verified
+    },
+  },
+  { timestamps: true } // Automatically add createdAt and updatedAt fields
+);
 module.exports = mongoose.model("User", UserSchema);
