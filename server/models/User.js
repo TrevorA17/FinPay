0280ec9f6e0d0b5b207b6ba165ec1822bba44e9e
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2025-01-30 10:42:37 +0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -9,5 +9,19 @@ const UserSchema = new mongoose.Schema({
     type: Boolean,
     default: false, // Initially, the user is not verified
   },
+  userAccounts: [
+    {
+      currency: { type: String, required: true },
+      bankName: { type: String, required: true },
+      accountNumber: { type: String, required: true },
+      accountName: { type: String, required: true },
+      country: { type: String, required: true },
+      streetAddress: { type: String, required: true },
+      state: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+    },
+    ,
+  ],
 });
 module.exports = mongoose.model("User", UserSchema);
