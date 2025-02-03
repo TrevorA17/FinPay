868bef5a0809938080c701f2ba1339c6f6a1e609
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2025-02-03 09:25:09 +0300

    Create ManageAccounts.jsx

---
+++ client/src/components/ManageAccounts.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import UserDropdown from "../components/UserDropdown";
+import AccountForm from "../components/AccountForm";
+import { Container, Typography } from "@mui/material";
+
+const ManageAccounts = () => {
+  const [selectedUserId, setSelectedUserId] = useState(null);
+
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Manage User Accounts
+      </Typography>
+      <UserDropdown onSelect={setSelectedUserId} />
+      {selectedUserId && <AccountForm userId={selectedUserId} />}
+    </Container>
+  );
+};
+
+export default ManageAccounts;
