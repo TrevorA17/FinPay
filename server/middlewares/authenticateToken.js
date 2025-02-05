5118606e1f1ff5757de0aaeb72ac5f37b088bc97
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2025-02-05 11:39:36 +0300

    Update authenticateToken.js

--- server/middlewares/authenticateToken.js
+++ server/middlewares/authenticateToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.header("Authorization");
-  console.log("Received Authorization Header:", authHeader);
+  // console.log("Received Authorization Header:", authHeader); debugging
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized: No token provided" });
@@ -13,7 +13,7 @@ const authenticateToken = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("Decoded User:", decoded);
+    // console.log("Decoded User:", decoded); debugging
     req.user = decoded; // Attach user data to the request object
     next(); // Proceed to the next middleware/route
   } catch (err) {
