a8a9dc1b65d30ed33a6d57a3a9d06184562dca63
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2025-02-19 11:28:32 +0300

    Update invoiceRoutes.js

--- server/routes/invoiceRoutes.js
+++ server/routes/invoiceRoutes.js
@@ -6,8 +6,11 @@ const {
 } = require("../controllers/invoiceController");
 const authenticateToken = require("../middlewares/authenticateToken");
 const { getCustomers } = require("../controllers/customerController");
+const { markInvoiceAsPaid } = require("../controllers/invoiceController");
 
 router.post("/", authenticateToken, createInvoice);
 router.get("/", authenticateToken, getInvoices);
+router.get("/", authenticateToken, getCustomers);
+router.post("/mark-as-paid/:id", markInvoiceAsPaid);
 
 module.exports = router;
