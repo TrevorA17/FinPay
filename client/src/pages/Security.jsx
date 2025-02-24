606ea2782a4052d73ad9b6f36d4cf1a1c8670c66
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2025-02-24 15:35:19 +0300

    Update Security.jsx

--- client/src/pages/Security.jsx
+++ client/src/pages/Security.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import {
   Box,
   ListItemIcon,
@@ -11,16 +12,17 @@ import {
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 
+//Quick action imports
 import SendMoney from "../components/SendMoney";
 import ConvertFunds from "../components/ConvertFunds";
 import CreateInvoice from "../components/CreateNewInvoice";
-import { useNavigate } from "react-router-dom";
 import CreateCustomer from "./../components/CreateNewCustomer";
 
 const Security = () => {
-  const navigate = useNavigate();
-  const [activePage, setActivePage] = useState(null); // Declare activePage state
   const [anchorEl, setAnchorEl] = useState(null);
+
+  const location = useLocation();
+  const navigate = useNavigate();
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -30,8 +32,8 @@ const Security = () => {
     setAnchorEl(null);
   };
 
-  const handleMenuClick = (page) => {
-    setActivePage(page); // Set the active page dynamically
+  const handleMenuClick = (action) => {
+    navigate(`/security?action=${action}`);
     handleClose(); // Close the dropdown
   };
 
@@ -39,152 +41,158 @@ const Security = () => {
     navigate("/"); // Go back to the main dashboard
   };
 
-  switch (activePage) {
-    case "Send Money":
-      return <SendMoney />;
-    case "Create Customer":
-      return <CreateCustomer />;
-    case "Convert Funds":
-      return <ConvertFunds />;
-    case "Create Invoice":
-      return <CreateInvoice />;
+  const searchParams = new URLSearchParams(location.search);
+  const activePage = searchParams.get("action");
 
-    default:
-      return (
-        <Box>
-          {/* Welcome Box */}
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              backgroundColor: "#fff",
-              padding: "38px",
-              boxShadow: "0 0.5px 0.5px rgba(0, 0, 0.0)",
-              marginBottom: "25px",
-              marginLeft: "-5px",
-            }}
-          >
-            <Typography variant="h4" sx={{ fontWeight: "bold" }}>
-              Security
-            </Typography>
-            <Button
-              variant="contained"
-              onClick={handleClick}
-              startIcon={<ArrowDropDownIcon />}
-              sx={{
-                backgroundColor: "#fff",
-                color: "#000",
-                textTransform: "none",
-              }}
-            >
-              Quick Actions
-            </Button>
-            <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={() => handleMenuClick("Send Money")}>
-                <ListItemIcon>
-                  <DashboardIcon fontSize="small" />
-                </ListItemIcon>
-                Send Money
-              </MenuItem>
-              <MenuItem onClick={() => handleMenuClick("Create Customer")}>
-                <ListItemIcon>
-                  <DashboardIcon fontSize="small" />
-                </ListItemIcon>
-                Create Customer
-              </MenuItem>
-              <MenuItem onClick={() => handleMenuClick("Convert Funds")}>
-                <ListItemIcon>
-                  <DashboardIcon fontSize="small" />
-                </ListItemIcon>
-                Convert Funds
-              </MenuItem>
-              <MenuItem onClick={() => handleMenuClick("Create Invoice")}>
-                <ListItemIcon>
-                  <DashboardIcon fontSize="small" />
-                </ListItemIcon>
-                Create New Invoice
-              </MenuItem>
-            </Menu>
-          </Box>
+  if (activePage) {
+    switch (activePage) {
+      case "send-money":
+        return <SendMoney />;
+      case "create-customer":
+        return <CreateCustomer />;
+      case "convert-funds":
+        return <ConvertFunds />;
+      case "create-invoice":
+        return <CreateInvoice />;
 
-          {/* Manage Security Box */}
-          <Box
-            sx={{
-              backgroundColor: "#fff",
-              padding: "30px", // Increased padding
-              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-              borderRadius: "8px",
-              marginBottom: "20px",
-              width: "80%", // Increased box size
-              marginLeft: "auto",
-              marginRight: "auto",
-            }}
-          >
-            <Typography
-              variant="h5"
-              sx={{ fontWeight: "bold", marginBottom: "20px" }}
-            >
-              Manage Security
-            </Typography>
-            <Divider sx={{ marginBottom: "20px" }} />
+      default:
+        break;
+    }
+  }
+  return (
+    <Box>
+      {/* Welcome Box */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          backgroundColor: "#fff",
+          padding: "38px",
+          boxShadow: "0 0.5px 0.5px rgba(0, 0, 0.0)",
+          marginBottom: "25px",
+          marginLeft: "-5px",
+        }}
+      >
+        <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+          Security
+        </Typography>
+        <Button
+          variant="contained"
+          onClick={handleClick}
+          startIcon={<ArrowDropDownIcon />}
+          sx={{
+            backgroundColor: "#fff",
+            color: "#000",
+            textTransform: "none",
+          }}
+        >
+          Quick Actions
+        </Button>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={() => handleMenuClick("send-money")}>
+            <ListItemIcon>
+              <DashboardIcon fontSize="small" />
+            </ListItemIcon>
+            Send Money
+          </MenuItem>
+          <MenuItem onClick={() => handleMenuClick("create-customer")}>
+            <ListItemIcon>
+              <DashboardIcon fontSize="small" />
+            </ListItemIcon>
+            Create Customer
+          </MenuItem>
+          <MenuItem onClick={() => handleMenuClick("convert-funds")}>
+            <ListItemIcon>
+              <DashboardIcon fontSize="small" />
+            </ListItemIcon>
+            Convert Funds
+          </MenuItem>
+          <MenuItem onClick={() => handleMenuClick("create-invoice")}>
+            <ListItemIcon>
+              <DashboardIcon fontSize="small" />
+            </ListItemIcon>
+            Create New Invoice
+          </MenuItem>
+        </Menu>
+      </Box>
 
-            {/* Password Section */}
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                paddingBottom: "20px",
-              }}
-            >
-              <Typography variant="h7" sx={{ marginBottom: "10px" }}>
-                Password <br />
-                Set a unique password to protect your account.
-              </Typography>
-              <Button variant="outlined" sx={{ alignSelf: "flex-start" }}>
-                Change Password <br />
-              </Button>
-            </Box>
-            <Divider sx={{ marginBottom: "20px" }} />
-            {/* Auth Method Section */}
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                paddingBottom: "20px",
-              }}
-            >
-              <Typography variant="h7" sx={{ marginBottom: "10px" }}>
-                Reset or Change Auth Method
-              </Typography>
-              <Button variant="outlined" sx={{ alignSelf: "flex-start" }}>
-                Activate 2FA
-              </Button>
-            </Box>
-            <Divider sx={{ marginBottom: "20px" }} />
-          </Box>
+      {/* Manage Security Box */}
+      <Box
+        sx={{
+          backgroundColor: "#fff",
+          padding: "30px", // Increased padding
+          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+          borderRadius: "8px",
+          marginBottom: "20px",
+          width: "80%", // Increased box size
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
+        <Typography
+          variant="h5"
+          sx={{ fontWeight: "bold", marginBottom: "20px" }}
+        >
+          Manage Security
+        </Typography>
+        <Divider sx={{ marginBottom: "20px" }} />
 
-          {/* Go Back Button */}
-          <Button
-            variant="outlined"
-            sx={{
-              marginTop: "20px",
-              backgroundColor: "#fff",
-              marginLeft: "65px",
-            }}
-            onClick={handleGoBack}
-          >
-            Go Back
+        {/* Password Section */}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingBottom: "20px",
+          }}
+        >
+          <Typography variant="h7" sx={{ marginBottom: "10px" }}>
+            Password <br />
+            Set a unique password to protect your account.
+          </Typography>
+          <Button variant="outlined" sx={{ alignSelf: "flex-start" }}>
+            Change Password <br />
           </Button>
         </Box>
-      );
-  }
+        <Divider sx={{ marginBottom: "20px" }} />
+        {/* Auth Method Section */}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingBottom: "20px",
+          }}
+        >
+          <Typography variant="h7" sx={{ marginBottom: "10px" }}>
+            Reset or Change Auth Method
+          </Typography>
+          <Button variant="outlined" sx={{ alignSelf: "flex-start" }}>
+            Activate 2FA
+          </Button>
+        </Box>
+        <Divider sx={{ marginBottom: "20px" }} />
+      </Box>
+
+      {/* Go Back Button */}
+      <Button
+        variant="outlined"
+        sx={{
+          marginTop: "20px",
+          backgroundColor: "#fff",
+          marginLeft: "65px",
+        }}
+        onClick={handleGoBack}
+      >
+        Go Back
+      </Button>
+    </Box>
+  );
 };
 
 export default Security;
